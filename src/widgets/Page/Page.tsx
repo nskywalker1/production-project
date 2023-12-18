797f5be9fbb0e6f10dfa6bf3d2c6891b5715ccfd
nazar <rachkovskiy.ua@gmail.com>
2023-12-18 18:41:49 +0200

    add virtualization for article list

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -18,6 +18,8 @@ interface PageProps {
     onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
     const { className, children, onScrollEnd } = props;
     const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -47,6 +49,7 @@ export const Page = memo((props: PageProps) => {
         <section
             ref={wrapperRef}
             onScroll={onScroll}
+            id={PAGE_ID}
             className={classNames(cls.Page, {}, [className])}
         >
             {children}
